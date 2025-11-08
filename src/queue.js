2299b616be0fcd686aa442760396ee41d7456cf6
Rostislav <rost.tm@gmail.com>
2025-11-08 10:59:32 +0300

    feat: solved 3 problems

--- src/queue.js
+++ src/queue.js
@@ -1,5 +1,5 @@
-const { NotImplementedError } = require('../lib/errors');
-// const { ListNode } = require('../extensions/list-node.js');
+// const { NotImplementedError } = require('../lib/errors');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -12,23 +12,65 @@ const { NotImplementedError } = require('../lib/errors');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
+
+// class ListNode {
+//   constructor(value) {
+//     this.value = value;
+//     this.next = null;
+//   }
+// }
 class Queue {
-  getUnderlyingList() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
-  }
+    constructor() {
+        this.end = null;
+        this.start = null;
+    }
+    enqueue(value) {
+        const node = new ListNode(value);
 
-  enqueue(/* value */) {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
-  }
+        if (!this.end) {
+            this.end = node;
+            this.start = node;
+        } else {
+            this.start.next = node;
+            this.start = node;
+        }
 
-  dequeue() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
-  }
+        return this.getUnderlyingList();
+    }
+
+    dequeue() {
+        if (!this.end) {
+            return undefined;
+        }
+
+        const removedValue = this.end.value;
+        this.end = this.end.next;
+
+        if (!this.end) {
+            this.start = null;
+        }
+
+        return removedValue;
+    }
+
+    getUnderlyingList() {
+        if (!this.end) return null;
+
+        let result = { value: this.end.value, next: null };
+        let currentResult = result;
+        let currentNode = this.end.next;
+
+        while (currentNode) {
+            currentResult.next = { value: currentNode.value, next: null };
+            currentResult = currentResult.next;
+            currentNode = currentNode.next;
+        }
+
+        return result;
+    }
 }
 
+
 module.exports = {
-  Queue
+    Queue
 };
